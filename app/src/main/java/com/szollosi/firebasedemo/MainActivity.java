d60324713a790c6321e952bed4617ac13e662b64
Istvan Szollosi <istvan.szollosi@yahoo.com>
2018-05-15 20:10:59 +0300

    First commit

---
+++ app/src/main/java/com/szollosi/firebasedemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.szollosi.firebasedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = MainActivity.class.getName();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Log.d(TAG, "[onCreate] Firebase token: " + FirebaseInstanceId.getInstance().getToken());
+  }
+}
